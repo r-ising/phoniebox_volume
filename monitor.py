cd9cea65f624d86f0cea8a078bef7ecd36d7825e
Ramon Ising <gitlab@ramonising.de>
2021-11-28 19:21:28 +0100

    Add implementation

--- monitor.py
+++ monitor.py
@@ -1,16 +1,66 @@
 #!/usr/bin/env python3
 
 from mpd import MPDClient
-client = MPDClient()               # create client object
-client.timeout = 10                # network timeout in seconds (floats allowed), default: None
-client.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
-client.connect("localhost", 6600)  # connect to localhost:6600
-print(client.mpd_version)          # print the MPD version
-print(client.find("any", "house")) # print result of the command "find any house"
-print(client.status())
-client.idle()
-print(client.status())
+from RPi import GPIO
 
-print(client.currentsong())
-client.close()                     # send the close command
-client.disconnect()                # disconnect from the server
+# LED pin mapping.
+red = 18
+green = 15
+blue = 14
+
+# GPIO setup.
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+
+# Set up colors using PWM so we can control individual brightness.
+RED = GPIO.PWM(red, 100)
+GREEN = GPIO.PWM(green, 100)
+BLUE = GPIO.PWM(blue, 100)
+RED.start(100)
+GREEN.start(100)
+BLUE.start(100)
+
+
+# Set a color by giving R, G, and B values of 0-100.
+def set_color(rgb=[]):
+    RED.ChangeDutyCycle(rgb[0])
+    GREEN.ChangeDutyCycle(rgb[1])
+    BLUE.ChangeDutyCycle(rgb[2])
+
+
+# volume 0 = 100 % green
+# volume 25 = 75 % green && 25 % red
+# volume 50 = 50% green && 50% red
+# volume 100 = 0% green & 100 % red
+def display_volume(volume):
+    max_file = "/home/pi/RPi-Jukebox-RFID/settings/Max_Volume_Limit"
+    vol = min(round(int(volume) / get_int(max_file) * 100), 100)
+    set_color([100 - vol, 100, vol])
+
+
+def get_int(file):
+    try:
+        with open(file) as f:
+            lines = f.readlines()
+
+        return int(lines[0])
+    except:
+        return -1
+
+
+client = MPDClient()
+client.timeout = 10
+client.idletimeout = None
+client.connect("localhost", 6600)
+
+while True:
+    display_volume(client.status()['volume'])
+    # wait for change
+    client.idle()
+
+client.close()
+client.disconnect()
--- systemd/install.sh
+++ systemd/install.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 cd "$(dirname "$0")"
+chmod +x /home/pi/phoniebox_volume/monitor.py
 sudo cp phoniebox_volume.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start phoniebox_volume
